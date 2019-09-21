04319c4264d3f090da7e172d619ff2d03a0578c0
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-21 23:04:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,36 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+
+let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  var konami = document.querySelector('body');
+  konami.addEventListener("keydown", function(event) {
+    onKeyDownHandler(event);
+  });
+}
 
+function onKeyDownHandler(e) {
+  const key = e.key;
+
+  if (key === codes[index]) {
+    index++;
+
+    if (index === codes.length) {
+      alert("Hurray!");
+      index = 0;
+   }
+  } else {
+    index = 0;
+  }
 }
